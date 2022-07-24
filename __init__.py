3c3d7dc08a4751b147ad7fd99f0ab8f0cc30eacf
snail911 <106596988+snail911@users.noreply.github.com>
2022-07-24 17:25:14 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .clean import create_folders
+from .clean import sort_files
+from .clean import normalize
+from .clean import new_names
+from .clean import extract_archives
+from .clean import del_empty_dirs

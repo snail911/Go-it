9b2eed5f0bc06404ed2121cbf9a9c90df400e0ad
snail911 <106596988+snail911@users.noreply.github.com>
2022-07-24 17:31:22 +0300

    Add files via upload

---
+++ clean_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='0.0.1',
+    description='My long code',
+    url='https://github.com/snail911/Go-it',
+    author='Yakiv KLymenko',
+    author_email='yasha1311@gmail.com',
+    license='Go-IT',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:clean']})
